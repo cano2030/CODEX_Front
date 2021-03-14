32ee64e8c1b545605634b2f45fe8a2e67e0e7b34
alejandra <mafa1008@gmail.com>
2021-03-14 12:02:58 -0500

    Aleja: primera edicion en el default

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,8 @@
 <template>
-  <v-app dark>
+  <v-app>
+    <!-- Menu hamburguesa -->
     <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
+      v-model="openMenu"
       fixed
       app
     >
@@ -24,66 +23,25 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+
+    <!--Barra de navegacion superior-->
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <!--Icono que controla el menú hamburguesa -->
+      <v-app-bar-nav-icon @click.stop="openMenu = !openMenu" />
     </v-app-bar>
     <v-main>
       <v-container>
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
       :absolute="!fixed"
       app
     >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-spacer></v-spacer>
+      <span>Codex &copy; {{ new Date().getFullYear() }}</span>
+      <small>Historias clinicas</small>
+    <v-spacer></v-spacer>
     </v-footer>
   </v-app>
 </template>
@@ -92,24 +50,24 @@
 export default {
   data () {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
+      openMenu: false,
       items: [
         {
           icon: 'mdi-apps',
-          title: 'Welcome',
+          title: 'Home',
           to: '/'
         },
         {
           icon: 'mdi-chart-bubble',
-          title: 'Inspire',
+          title: 'Iniciar sesion',
           to: '/inspire'
+        },
+        {
+          icon: 'mdi-cigar',
+          title: 'Sobre nosotros',
+          to: '/about'
         }
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
       title: 'Vuetify.js'
     }
   }
