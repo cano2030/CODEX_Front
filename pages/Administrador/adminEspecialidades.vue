66070ab8663c542843063423f13402824e717b32
yesenialg <yesenialopezg07@gmail.com>
2021-03-23 17:46:30 -0500

    Yesenia: PantallasAdmin Medicamentos-Especialidades

--- pages/Administrador/adminEspecialidades.vue
+++ pages/Administrador/adminEspecialidades.vue
@@ -1,13 +1,126 @@
 <template>
-  
+  <v-template>
+    <center>
+      <v-card style="width: 800px" elevation="12">
+        <v-toolbar color="#a4ebf3">
+          <v-card-title class="#0c354a--text">
+            <center>Agregar especialidad</center>
+          </v-card-title>
+        </v-toolbar>
+        <v-card-text>
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field
+              v-model="especialidad"
+              :rules="rules.required"
+              label="Nombre Especialidad"
+              style="height: 100px"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="descripcion"
+              label="Descripcion"
+              style="height: 100px"
+            ></v-text-field>
+            <center>
+              <v-btn class="white--text" color="#ff5722" @click="validate">
+                Agregar
+              </v-btn>
+              <v-btn
+                color="#0c354a"
+                class="ma-1"
+                v-bind="attrs"
+                v-on="on"
+                plain
+              >
+                Eliminar
+              </v-btn>
+            </center>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </center>
+    <v-container>
+      <v-card>
+        <v-card-title>
+          <v-icon large left> </v-icon>
+
+          <div>
+            <strong class="#0c354a">Buscar Especialidad</strong>
+          </div>
+        </v-card-title>
+
+        <v-toolbar flat color="transparent">
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Buscar especialidad"
+            single-line
+          ></v-text-field>
+        </v-toolbar>
+
+        <v-list three-line color="#f4f9f9">
+          <v-list-item
+            v-for="(item, i) in searching"
+            :key="i"
+            ripple
+            @click="() => {}"
+          >
+            <v-list-item-content>
+              <div v-text="item.id"></div>
+              <div v-text="item.nombre"></div>
+              <span
+                class="text-uppercase font-weight-regular caption"
+                v-text="item.descripcion"
+              ></span>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-container>
+  </v-template>
 </template>
 
 <script>
 export default {
-        layout: "admin",
-}
+  layout: "admin",
+  data: () => ({
+    rules: {
+      required: [(v) => !!v || "El campo es obligatorio"],
+    },
+    items: [
+      {
+        nombre: "General",
+        id: "111",
+        descripcion: "",
+      },
+      {
+        nombre: "Pediatria",
+        id: "222",
+        descripcion: "",
+      },
+    ],
+    search: "",
+  }),
+  methods: {
+    validate() {
+      this.$refs.form.validate();
+    },
+  },
+  computed: {
+    searching() {
+      if (!this.search) return this.items;
+
+      const search = this.search.toLowerCase();
+
+      return this.items.filter((item) => {
+        const text = item.id.toLowerCase();
+
+        return text.indexOf(search) > -1;
+      });
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
--- pages/Administrador/adminHistoriaClinica.vue
+++ pages/Administrador/adminHistoriaClinica.vue
@@ -1,13 +1,157 @@
 <template>
-  
+  <v-container>
+    <v-card>
+      <v-card-title>
+        <v-icon large left> </v-icon>
+
+        <div>
+          <strong class="#0c354a">Buscar Usuarios</strong>
+        </div>
+      </v-card-title>
+
+      <v-toolbar flat color="transparent">
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Buscar Usuarios"
+          single-line
+        ></v-text-field>
+      </v-toolbar>
+
+      <v-list three-line color="#f4f9f9">
+        <v-list-item dense true 
+          v-for="(item, i) in searching"
+          :key="i"
+          ripple
+          @click="() => {}"
+        >
+          <v-img :src="item.image" class="mr-4" max-width="64" min-width="64">
+          </v-img>
+
+          <v-list-item-content>
+            <div v-text="item.rol"></div>
+            <span
+              class="text-uppercase font-weight-regular caption"
+              v-text="item.nombre"
+            ></span>
+            <div v-text="item.documento"></div>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 export default {
-        layout: "admin",
-}
+  layout: "admin",
+  data: () => ({
+    rules: {
+      required: [(v) => !!v || "El campo es obligatorio"],
+    },
+      country: null,
+    items: [
+      {
+        image: "https://randomuser.me/api/portraits/women/10.jpg",
+        nombre: "Mariana Palacios Hinestroza",
+        documento: "123456789",
+        rol: "Paciente",
+      },
+      {
+        image: "https://randomuser.me/api/portraits/women/20.jpg",
+        nombre: "Yesenia Lopez Giraldo",
+        documento: "1365478216",
+        rol: "Auxiliar",
+      },
+      {
+        image: "https://randomuser.me/api/portraits/women/30.jpg",
+        nombre: "Maria Alejandra Franco Alzate",
+        documento: "112365749",
+        rol: "Medico",
+      },
+      {
+        image: "https://randomuser.me/api/portraits/women/40.jpg",
+        nombre: "Angie Rivera Hinestroza",
+        documento: "114796632",
+        rol: "Paciente",
+      },
+      {
+        image: "https://randomuser.me/api/portraits/women/50.jpg",
+        nombre: "Dayana Mosquera Mosquera",
+        documento: "974632114",
+        rol: "Medico",
+      },
+      {
+        image: "https://randomuser.me/api/portraits/women/60.jpg",
+        nombre: "Mariana Palacios Hinestroza",
+        documento: "123456789",
+        rol: "Paciente",
+      },
+      {
+        image: "https://randomuser.me/api/portraits/men/10.jpg",
+        nombre: "Mariana Palacios Hinestroza",
+        documento: "123456789",
+        rol: "Auxiliar",
+      },
+      {
+        image: "https://randomuser.me/api/portraits/men/10.jpg",
+        nombre: "Mariana Palacios Hinestroza",
+        documento: "123456789",
+        rol: "Medico",
+      },
+      {
+        image: "https://randomuser.me/api/portraits/men/10.jpg",
+        nombre: "Mariana Palacios Hinestroza",
+        documento: "123456789",
+        rol: "Paciente",
+      },
+      {
+        image: "https://randomuser.me/api/portraits/men/10.jpg",
+        nombre: "Mariana Palacios Hinestroza",
+        documento: "123456789",
+        rol: "Paciente",
+      },
+      {
+        image: "https://randomuser.me/api/portraits/men/10.jpg",
+        nombre: "Mariana Palacios Hinestroza",
+        documento: "123456789",
+        rol: "Paciente",
+      },
+      {
+        image: "https://randomuser.me/api/portraits/men/10.jpg",
+        nombre: "Mariana Palacios Hinestroza",
+        documento: "123456789",
+        rol: "Paciente",
+      },
+      {
+        image: "https://randomuser.me/api/portraits/men/10.jpg",
+        nombre: "Mariana Palacios Hinestroza",
+        documento: "123456789",
+        rol: "Paciente",
+      },
+    ],
+    search: "",
+  }),
+  methods: {
+    validate() {
+      this.$refs.form.validate();
+    },
+  },
+  computed: {
+    searching() {
+      if (!this.search) return this.items;
+
+      const search = this.search.toLowerCase();
+
+      return this.items.filter((item) => {
+        const text = item.documento.toLowerCase();
+
+        return text.indexOf(search) > -1;
+      });
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
