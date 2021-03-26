b24b9ec799b7744d59f8ef2533646d3b4f03d3a7
cano2030 <cano2030@gmail.com>
2021-03-26 01:14:08 -0500

    santi: Casi todo, falta enrutamiento login q no dio

--- layouts/medico.vue
+++ layouts/medico.vue
@@ -1,17 +1,24 @@
 <template>
   <v-app>
     <!-- Menu hamburguesa -->
-    <v-navigation-drawer v-model="openMenu" class="blue lighten-3" fixed app>
+    <v-navigation-drawer v-model="openMenu" color="#a4ebf3" fixed app>
       <template v-slot:prepend>
         <v-list-item two-line>
           <v-avatar size="180">
-            <v-img src="https://randomuser.me/api/portraits/women/21.jpg"></v-img>
+            <v-img
+              src="https://res.cloudinary.com/postedin/image/upload/d_psu:no-image.jpg,f_auto,q_80/psu/c-postedin-image-65344.jpeg"
+             
+            ></v-img>
           </v-avatar>
         </v-list-item>
 
         <v-list-item-content>
-          <v-list-item-title class="title"> Alma Park </v-list-item-title>
-          <v-list-item-subtitle>Doctor</v-list-item-subtitle>
+          <v-list-item-title class="text-md-center">
+            Karen Johana Mosquera
+          </v-list-item-title>
+          <v-list-item-subtitle class="text-md-center"
+            >Auxiliar de Enfermeria</v-list-item-subtitle
+          >
         </v-list-item-content>
       </template>
 
@@ -34,8 +41,8 @@
 
       <template v-slot:append>
         <div>
-          <v-btn block>
-            Cerrar sesión
+          <v-btn block text to="/">
+            Logout
             <v-icon> mdi-exit-to-app </v-icon>
           </v-btn>
         </div>
@@ -43,9 +50,17 @@
     </v-navigation-drawer>
 
     <!--Barra de navegacion superior-->
-    <v-app-bar :clipped-left="clipped" fixed app>
+    <v-app-bar :clipped-left="clipped" fixed app color="#ccf2f4">
       <!--Icono que controla el menú hamburguesa -->
       <v-app-bar-nav-icon @click.stop="openMenu = !openMenu" />
+
+      <v-toolbar-items>
+        <img
+          src="../static/images/logoapolo.PNG"
+          max-width="64"
+          min-width="64"
+        />
+      </v-toolbar-items>
     </v-app-bar>
     <v-main>
       <v-container>
@@ -68,10 +83,16 @@ export default {
       openMenu: false,
       items: [
         {
-          id: "perfil",
+          id: "homeauxiliar",
+          icon: "mdi-home",
+          title: "Home",
+          to: "/Medico/MedicoHome",
+        },
+        {
+          id: "perfilauxiliar",
           icon: "mdi-account",
           title: "Perfil",
-          to: "/Usuario/MedicoPerfil",
+          to: "/Medico/MedicoPerfil",
         },
       ],
       title: "Vuetify.js",
